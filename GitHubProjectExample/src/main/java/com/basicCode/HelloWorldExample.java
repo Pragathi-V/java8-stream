d5df70da8ae1922732f4a30f5b8c34a7ea5bcf45
Pragathi <pragathivadigeri3@gmail.com>
2022-04-21 10:45:25 +0530

    basic eclipse code

---
+++ GitHubProjectExample/src/main/java/com/basicCode/HelloWorldExample.java
@@ -0,0 +1,12 @@
+package com.basicCode;
+
+public class HelloWorldExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi I am from Raichur");
+		System.out.println("I am learning java");
+	}
+
+}
